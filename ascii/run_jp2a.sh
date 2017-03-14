cb84ff520c1f138504f89122b70b46937feb3bfb
Christina Koch <christinakconnect@gmail.com>
2017-03-14 11:15:28 -0700

    Update run_jp2a.sh

--- ascii/run_jp2a.sh
+++ ascii/run_jp2a.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #untar
+tar czf jp2a_pkg.tgz
 #set LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$_CONDOR_SCRATCH_DIR:$LD_LIBRARY_PATH
 #set PATH
+export PATH=$CONDOR_SCRATCH_DIR:$PATH
 #run jp2a
+jp2a $1
