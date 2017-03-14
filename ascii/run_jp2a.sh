9b5ae8a478153fb265f0e9913ed5643f73c93da9
Christina Koch <christina.koch@ldas-osg.ligo.caltech.edu>
2017-03-14 13:56:42 -0700

    full wrapper script

--- ascii/run_jp2a.sh
+++ ascii/run_jp2a.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export TERM=xterm-256color
 #untar
-tar czf jp2a_pkg.tgz
+tar xzf jp2a_pkg.tgz
 #set LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$_CONDOR_SCRATCH_DIR:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$_CONDOR_SCRATCH_DIR/jpeglib/lib:$LD_LIBRARY_PATH
 #set PATH
-export PATH=$CONDOR_SCRATCH_DIR:$PATH
+export PATH=$_CONDOR_SCRATCH_DIR/jp2a/bin:$PATH
 #run jp2a
 jp2a $1
